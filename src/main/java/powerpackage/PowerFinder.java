28ddc44333389d81e42198d4136a3c6e1eb69644
Ranjeeta Sharma <61404441+RanjeetaSharma@users.noreply.github.com>
2022-03-04 22:47:37 +0530

    Update and rename powerfinder.java to PowerFinder.java

---
+++ src/main/java/powerpackage/PowerFinder.java
@@ -0,0 +1,17 @@
+package powerpackage;
+public static class PowerFinder {
+    public static void main(String[] args) {
+
+        public static int powerOf (int base = 3, exponent = 4) {
+
+            long result = 1;
+
+            for (; exponent != 0; --exponent) {
+                result *= base;
+            }
+
+            System.out.println("Answer = " + result);
+        }
+
+    }
+}
